0e302a471637097e89d2d78fb0e7117694151395
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-20 17:11:24 +0530

    added the no args and a parameterised constructor

--- src/main/java/com/niit/jdp/model/Song.java
+++ src/main/java/com/niit/jdp/model/Song.java
@@ -8,4 +8,17 @@ public class Song {
     private String artistName;
     private String albumName;
     private String genre;
+
+    public Song() {
+    }
+
+    public Song(int id, String name, int durationInSeconds, String url, String artistName, String albumName, String genre) {
+        this.id = id;
+        this.name = name;
+        this.durationInSeconds = durationInSeconds;
+        this.url = url;
+        this.artistName = artistName;
+        this.albumName = albumName;
+        this.genre = genre;
+    }
 }
