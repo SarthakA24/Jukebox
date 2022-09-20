6c627fb3138e401f8634b065c56bee8cc3b5f019
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-20 23:46:40 +0530

    added java docs for the methods in JukeboxService.java

--- src/main/java/com/niit/jdp/service/JukeboxService.java
+++ src/main/java/com/niit/jdp/service/JukeboxService.java
@@ -3,23 +3,52 @@ package com.niit.jdp.service;
 import com.niit.jdp.model.Playlist;
 import com.niit.jdp.model.Song;
 
-public class JukeboxService {
-    public void displayAllSongs() {
+import java.util.List;
 
+public class JukeboxService {
+    /**
+     * This method is used to display all the songs from the database
+     *
+     * @param songsList The list of songs to be displayed.
+     */
+    public void displayAllSongs(List<Song> songsList) {
     }
 
+    /**
+     * This method is used to display details of a particular song
+     *
+     * @param song The song whose details are to be displayed.
+     */
     public void displayAllSongs(Song song) {
 
     }
 
+    /**
+     * This method is used to play a song from the playlist
+     *
+     * @param playlist The playlist from which the song is to be played.
+     */
     public void playSong(Playlist playlist) {
 
     }
 
+    /**
+     * This method is used to play the current paused song
+     */
+    public void playSong() {
+
+    }
+
+    /**
+     * This method is used to pause a currently playing song
+     */
     public void pauseSong() {
 
     }
 
+    /**
+     * This method is used to shuffle the playlist and play the songs of the playlist in a random order
+     */
     public void shufflePlaylist() {
 
     }
