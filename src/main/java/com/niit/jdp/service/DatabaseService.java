c8196565a55cc0f91764214b39d0852eb3ceaac6
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-20 17:45:27 +0530

    added JavaDocs for the methods connect() and printConnectionStatus()

--- src/main/java/com/niit/jdp/service/DatabaseService.java
+++ src/main/java/com/niit/jdp/service/DatabaseService.java
@@ -18,10 +18,18 @@ public class DatabaseService {
         this.connection = null;
     }
 
+    /**
+     * This method establishes a connection between the program and the database
+     *
+     * @throws SQLException If the database URL or USERNAME or PASSWORD are incorrect
+     */
     public void connect() throws SQLException {
         connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
 
+    /**
+     * This method prints if the database is connected or not
+     */
     public void printConnectionStatus() {
         if (connection != null) System.out.println("\u001B[32mDatabase is connected.\u001B[0m");
         else System.err.println("!!!Database is NOT connected!!!");
