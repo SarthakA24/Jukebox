0522d00babe113c9170a27f8b15c47cfaef40614
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-20 17:40:13 +0530

    added the private static final fields for database URL, USERNAME and PASSWORD, and added the field for Connection object

--- src/main/java/com/niit/jdp/service/DatabaseService.java
+++ src/main/java/com/niit/jdp/service/DatabaseService.java
@@ -1,4 +1,10 @@
 package com.niit.jdp.service;
 
+import java.sql.Connection;
+
 public class DatabaseService {
+    private static final String URL = "jdbc:mysql://localhost:3306/jukebox";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "admin";
+    private Connection connection;
 }
