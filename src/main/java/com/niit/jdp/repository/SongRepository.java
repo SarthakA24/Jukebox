1c4253b631a96782895239d650ad10bca5034cc8
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-20 17:22:04 +0530

    added the method signature for getAllSongs()

--- src/main/java/com/niit/jdp/repository/SongRepository.java
+++ src/main/java/com/niit/jdp/repository/SongRepository.java
@@ -3,6 +3,7 @@ package com.niit.jdp.repository;
 import com.niit.jdp.model.Song;
 
 import java.sql.Connection;
+import java.util.List;
 
 public class SongRepository {
     public boolean addSongToCatalog(Connection connection, Song song) {
@@ -11,5 +12,9 @@ public class SongRepository {
 
     public boolean deleteSongFromCatalog(Connection connection, int songId) {
         return false;
+    }
+
+    public List<Song> getAllSongs(Connection connection) {
+        return List.of();
     }
 }
