bd9c0601ee456705cbc54a4de5e2b581c5e71928
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-20 17:23:36 +0530

    added the method signatures for searching songs by artist, genre and name

--- src/main/java/com/niit/jdp/repository/SongRepository.java
+++ src/main/java/com/niit/jdp/repository/SongRepository.java
@@ -17,4 +17,16 @@ public class SongRepository {
     public List<Song> getAllSongs(Connection connection) {
         return List.of();
     }
+
+    public List<Song> searchSongsByArtist(Connection connection, String artistName) {
+        return List.of();
+    }
+
+    public List<Song> searchSongsByGenre(Connection connection, String genre) {
+        return List.of();
+    }
+
+    public List<Song> searchSongsByName(Connection connection, String songName) {
+        return List.of();
+    }
 }
