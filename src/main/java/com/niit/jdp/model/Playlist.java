b9b042862e4cdc357dfb71f8ca6a75f8676cf864
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-20 17:16:01 +0530

    added the no args and the parametrised constructors

--- src/main/java/com/niit/jdp/model/Playlist.java
+++ src/main/java/com/niit/jdp/model/Playlist.java
@@ -1,8 +1,18 @@
 package com.niit.jdp.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Playlist {
     private String playlistName;
     private List<Song> songsList;
+
+    public Playlist() {
+        this.songsList = new ArrayList<>();
+    }
+
+    public Playlist(String playlistName, List<Song> songsList) {
+        this.playlistName = playlistName;
+        this.songsList = songsList;
+    }
 }
