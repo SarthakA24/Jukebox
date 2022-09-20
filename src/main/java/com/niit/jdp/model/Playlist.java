2f90fb0f01801c7f722c0fbab4232fa736c97bf4
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-20 17:16:22 +0530

    added the getter and setter methods

--- src/main/java/com/niit/jdp/model/Playlist.java
+++ src/main/java/com/niit/jdp/model/Playlist.java
@@ -15,4 +15,20 @@ public class Playlist {
         this.playlistName = playlistName;
         this.songsList = songsList;
     }
+
+    public String getPlaylistName() {
+        return playlistName;
+    }
+
+    public void setPlaylistName(String playlistName) {
+        this.playlistName = playlistName;
+    }
+
+    public List<Song> getSongsList() {
+        return songsList;
+    }
+
+    public void setSongsList(List<Song> songsList) {
+        this.songsList = songsList;
+    }
 }
