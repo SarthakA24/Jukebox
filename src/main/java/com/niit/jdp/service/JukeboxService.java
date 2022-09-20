970a004da7baf7e7201919ef50b3e70726a07355
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-20 17:35:25 +0530

    added the method signatures for displayAllSongs(), displayAllSongs(), playSong(), pauseSong(), shufflePlaylist()

--- src/main/java/com/niit/jdp/service/JukeboxService.java
+++ src/main/java/com/niit/jdp/service/JukeboxService.java
@@ -1,4 +1,26 @@
 package com.niit.jdp.service;
 
+import com.niit.jdp.model.Playlist;
+import com.niit.jdp.model.Song;
+
 public class JukeboxService {
+    public void displayAllSongs() {
+
+    }
+
+    public void displayAllSongs(Song song) {
+
+    }
+
+    public void playSong(Playlist playlist) {
+
+    }
+
+    public void pauseSong() {
+
+    }
+
+    public void shufflePlaylist() {
+
+    }
 }
