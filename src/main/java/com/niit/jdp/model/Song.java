6698d284c2087d4ae64559191e99f8d630403fc6
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-20 17:12:09 +0530

    added the equals() and hashCode() methods

--- src/main/java/com/niit/jdp/model/Song.java
+++ src/main/java/com/niit/jdp/model/Song.java
@@ -1,5 +1,7 @@
 package com.niit.jdp.model;
 
+import java.util.Objects;
+
 public class Song {
     private int id;
     private String name;
@@ -76,5 +78,18 @@ public class Song {
 
     public void setGenre(String genre) {
         this.genre = genre;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Song)) return false;
+        Song song = (Song) o;
+        return getId() == song.getId() && getDurationInSeconds() == song.getDurationInSeconds() && Objects.equals(getName(), song.getName()) && Objects.equals(getUrl(), song.getUrl()) && Objects.equals(getArtistName(), song.getArtistName()) && Objects.equals(getAlbumName(), song.getAlbumName()) && Objects.equals(getGenre(), song.getGenre());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getName(), getDurationInSeconds(), getUrl(), getArtistName(), getAlbumName(), getGenre());
     }
 }
