da8a5fa7c3befec95cdd91ffad8cb741dd6a1f43
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-20 17:10:45 +0530

    added the fields for Song class

--- src/main/java/com/niit/jdp/model/Song.java
+++ src/main/java/com/niit/jdp/model/Song.java
@@ -1,4 +1,11 @@
 package com.niit.jdp.model;
 
 public class Song {
+    private int id;
+    private String name;
+    private int durationInSeconds;
+    private String url;
+    private String artistName;
+    private String albumName;
+    private String genre;
 }
