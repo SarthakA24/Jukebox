91d38ed7a5bbad93f3f966090b0933340318f404
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-20 17:25:34 +0530

    added the method signature for createNewPlaylist()

--- src/main/java/com/niit/jdp/repository/PlaylistRepository.java
+++ src/main/java/com/niit/jdp/repository/PlaylistRepository.java
@@ -1,4 +1,9 @@
 package com.niit.jdp.repository;
 
+import java.sql.Connection;
+
 public class PlaylistRepository {
+    public boolean createNewPlaylist(Connection connection, String playlistName) {
+        return false;
+    }
 }
