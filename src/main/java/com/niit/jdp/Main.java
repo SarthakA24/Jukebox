21d8bca86e3ded784c0b160f8e573f4d29de2667
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-21 17:38:30 +0530

    commented the if else in main

--- src/main/java/com/niit/jdp/Main.java
+++ src/main/java/com/niit/jdp/Main.java
@@ -1,6 +1,5 @@
 package com.niit.jdp;
 
-import com.niit.jdp.model.Song;
 import com.niit.jdp.repository.PlaylistRepository;
 import com.niit.jdp.repository.SongRepository;
 import com.niit.jdp.service.DatabaseService;
@@ -8,7 +7,6 @@ import com.niit.jdp.service.JukeboxService;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -33,14 +31,37 @@ public class Main {
                 // display the jukebox menu
                 jukeboxService.displayMenu();
                 choice = scanner.nextInt();
-                if (choice == 1) {
-                    List<Song> allSongs = songRepository.getAllSongs(connection);
-                    songRepository.displayAllSongs(allSongs);
-                } else if (choice == 2) {
-                    List<String> playlistsName = playlistRepository.getPlaylistsName(connection);
-                    String[] playlistNames = new String[playlistsName.size()];
-                    playlistsName.toArray(playlistNames);
-                }
+//                if (choice == 1) {
+//                    List<Song> allSongs = songRepository.getAllSongs(connection);
+//                    songRepository.displayAllSongs(allSongs);
+//                } else if (choice == 2) {
+//                    System.out.println("Enter the playlist name - ");
+//                    scanner.nextLine();
+//                    String playlistName = scanner.nextLine();
+//                    boolean result = playlistRepository.createNewPlaylist(connection, playlistName);
+//                    if (result) {
+//                        System.out.println("Playlist Created Successfully!");
+//                    } else {
+//                        System.err.println("Playlist with the name " + playlistName + " already exists!");
+//                    }
+//                } else if (choice == 3) {
+//                    List<String> playlistsName = playlistRepository.getPlaylistsName(connection);
+//                    System.out.println("Your Playlists are -");
+//                    if (playlistsName.size() == 0) {
+//                        System.err.println("There are no playlists created!!");
+//                    } else {
+//                        for (int index = 0; index < playlistsName.size(); index++) {
+//                            System.out.println(index + 1 + ". " + playlistsName.get(index));
+//                        }
+//                        System.out.println("Please enter the playlist name that you need to play - ");
+//                        String selectedPlaylist = scanner.nextLine();
+//                        if (playlistsName.contains(selectedPlaylist)) {
+//                            int index = playlistsName.indexOf(selectedPlaylist);
+//                        } else {
+//                            System.err.println("Incorrect playlist name!!");
+//                        }
+//                    }
+//                }
             } while (choice != 6);
         } catch (SQLException e) {
             System.err.println(e.getMessage());
