c76e7cfc42d4b1af3e99572598d93858c605985d
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-21 19:23:15 +0530

    added sql queries to alter the table

--- src/main/resources/sql/playlist_sql_queries.sql
+++ src/main/resources/sql/playlist_sql_queries.sql
@@ -9,3 +9,15 @@ CREATE TABLE IF NOT EXISTS `jukebox`.`playlist`
     `list_of_songs` VARCHAR(45) NULL,
     PRIMARY KEY (`playlist_name`)
 );
+
+-- alter the table to include new fields
+ALTER TABLE `jukebox`.`playlist`
+    ADD COLUMN `playlist_id` INT NOT NULL FIRST,
+    CHANGE COLUMN `list_of_songs` `songs_id` VARCHAR(45) NULL DEFAULT NULL,
+    DROP PRIMARY KEY,
+    ADD PRIMARY KEY (`playlist_id`);
+;
+
+-- make the playlist id auto increment
+ALTER TABLE `jukebox`.`playlist`
+    CHANGE COLUMN `playlist_id` `playlist_id` INT NOT NULL AUTO_INCREMENT;
