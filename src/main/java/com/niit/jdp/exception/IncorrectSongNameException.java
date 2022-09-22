53baefab10944dc458192fbb014369b019923a2a
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-22 15:36:57 +0530

    added the exception IncorrectSongNameException()

---
+++ src/main/java/com/niit/jdp/exception/IncorrectSongNameException.java
@@ -0,0 +1,7 @@
+package com.niit.jdp.exception;
+
+public class IncorrectSongNameException extends Exception {
+    public IncorrectSongNameException(String message) {
+        super(message);
+    }
+}
