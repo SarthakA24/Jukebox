beec032793ecf581eb4e355ef57e7578b70a7a2a
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-22 13:16:08 +0530

    added test case for checking database connection

--- src/test/java/com/niit/jdp/service/DatabaseServiceTest.java
+++ src/test/java/com/niit/jdp/service/DatabaseServiceTest.java
@@ -1,20 +1,27 @@
 package com.niit.jdp.service;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLException;
+
 class DatabaseServiceTest {
+    DatabaseService databaseService;
 
     @BeforeEach
     void setUp() {
+        databaseService = new DatabaseService();
     }
 
     @AfterEach
     void tearDown() {
+        databaseService = null;
     }
 
     @Test
-    void connect() {
+    void databaseConnectionSuccess() throws SQLException {
+        Assertions.assertTrue(databaseService.connect());
     }
 }
