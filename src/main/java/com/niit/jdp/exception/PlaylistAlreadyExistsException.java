57dde1462ba26fe5741d67279d5ad24695eb4cb8
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-22 15:24:16 +0530

    added the exception class PlaylistAlreadyExistsException

---
+++ src/main/java/com/niit/jdp/exception/PlaylistAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.niit.jdp.exception;
+
+public class PlaylistAlreadyExistsException extends Exception {
+
+    public PlaylistAlreadyExistsException(String message) {
+        super(message);
+    }
+}
