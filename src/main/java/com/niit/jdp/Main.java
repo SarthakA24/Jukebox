364b8e10dd6ce49fab2ccabd000550a3e8a77eff
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-22 15:12:36 +0530

    removed the unnecessary if conditions from the main method

--- src/main/java/com/niit/jdp/Main.java
+++ src/main/java/com/niit/jdp/Main.java
@@ -37,110 +37,82 @@ public class Main {
                     jukeboxService.displayMenu();
                     choice = scanner.nextInt();
                     List<Song> allSongs = songRepository.getAllSongs(connection);
-                    if (choice == 1) {
-                        songRepository.displayAllSongs(allSongs);
-                    } else if (choice == 2) {
-                        System.out.println("Enter the playlist name - ");
-                        scanner.nextLine();
-                        String playlistName = scanner.nextLine();
-                        boolean result = playlistRepository.createNewPlaylist(connection, playlistName);
-                        if (result) {
-                            System.out.println("Playlist Created Successfully!");
-                        } else {
-                            System.err.println("Playlist with the name " + playlistName + " already exists!");
+                    switch (choice) {
+                        case 1:
+                            songRepository.displayAllSongs(allSongs);
+                            break;
+                        case 2: {
+                            System.out.println("Enter the playlist name you want to create - ");
+                            scanner.nextLine();
+                            String playlistName = scanner.nextLine();
+                            playlistRepository.createNewPlaylist(connection, playlistName);
+                            break;
                         }
-                    } else if (choice == 3) {
-                        List<String> playlistsName = playlistRepository.getPlaylistsName(connection);
-                        System.out.println("Your Playlists are -");
-                        if (playlistsName.size() == 0) {
-                            System.err.println("There are no playlists created!!");
-                        } else {
+                        case 3: {
+                            List<String> playlistsName = playlistRepository.getPlaylistsName(connection);
+                            System.out.println("Your Playlists are -");
                             IntStream.range(0, playlistsName.size()).mapToObj(index -> index + 1 + ". " + playlistsName.get(index)).forEach(System.out::println);
                             System.out.println("Please enter the playlist name that you need to play - ");
                             scanner.nextLine();
                             String selectedPlaylist = scanner.nextLine().toLowerCase();
-                            if (playlistsName.contains(selectedPlaylist)) {
-                                Playlist playlistByName = playlistRepository.getPlaylistByName(connection, selectedPlaylist);
-                                jukeboxService.playPlaylist(playlistByName);
-                            } else {
-                                System.err.println("Incorrect playlist name!!");
-                            }
+                            Playlist playlistByName = playlistRepository.getPlaylistByName(connection, selectedPlaylist);
+                            jukeboxService.playPlaylist(playlistByName);
+                            break;
                         }
-                    } else if (choice == 4) {
-                        System.out.println("Enter the playlist name - ");
-                        scanner.nextLine();
-                        String playlistName = scanner.nextLine();
-                        List<String> playlistsName = playlistRepository.getPlaylistsName(connection);
-                        if (playlistsName.contains(playlistName)) {
-                            System.out.println("Songs in the Jukebox - ");
-                            songRepository.displayAllSongs(allSongs);
-                            System.out.println("Enter the song name to add to the playlist - ");
-                            String songName = scanner.nextLine();
-                            Song song = songRepository.searchSongsByName(allSongs, songName);
-                            boolean result = playlistRepository.addSongToPlaylist(connection, song, playlistName);
-                            if (result) {
-                                System.out.println("Song added to the playlist successfully!");
+                        case 4: {
+                            System.out.println("Enter the playlist name in which song is to be added - ");
+                            scanner.nextLine();
+                            String playlistName = scanner.nextLine();
+                            List<String> playlistsName = playlistRepository.getPlaylistsName(connection);
+                            if (playlistsName.contains(playlistName)) {
+                                System.out.println("Songs in the Jukebox - ");
+                                songRepository.displayAllSongs(allSongs);
+                                System.out.println("Enter the song name to add to the playlist - ");
+                                String songName = scanner.nextLine();
+                                Song song = songRepository.searchSongsByName(allSongs, songName);
+                                playlistRepository.addSongToPlaylist(connection, song, playlistName);
                             } else {
-                                System.err.println("Incorrect song name!");
+                                System.err.println("Playlist does not exist!");
                             }
-                        } else {
-                            System.err.println("Playlist with the name " + playlistName + " does not exist!");
+                            break;
                         }
-                    } else if (choice == 5) {
-                        System.out.println("Enter the playlist name - ");
-                        scanner.nextLine();
-                        String playlistName = scanner.nextLine();
-                        List<String> playlistsName = playlistRepository.getPlaylistsName(connection);
-                        if (playlistsName.contains(playlistName)) {
+                        case 5: {
+                            System.out.println("Enter the playlist name - ");
+                            scanner.nextLine();
+                            String playlistName = scanner.nextLine();
                             Playlist playlistByName = playlistRepository.getPlaylistByName(connection, playlistName);
                             playlistRepository.displayPlaylist(playlistByName);
                             System.out.println("Enter the name of the song to remove - ");
                             String songName = scanner.nextLine();
                             Song songToRemove = songRepository.searchSongsByName(allSongs, songName);
-                            boolean result = playlistRepository.removeSongFromPlaylist(connection, songToRemove, playlistName);
-                            if (result) {
-                                System.out.println("Song removed from the playlist successfully!");
-                            } else {
-                                System.err.println("Incorrect song name!");
-                            }
-                        } else {
-                            System.err.println("Playlist with the name " + playlistName + " does not exist!");
+                            playlistRepository.removeSongFromPlaylist(connection, songToRemove, playlistName);
+                            break;
                         }
-                    } else if (choice == 6) {
-                        System.out.println("Enter the playlist name you want to edit - ");
-                        scanner.nextLine();
-                        String oldPlaylistName = scanner.nextLine();
-                        List<String> playlistsName = playlistRepository.getPlaylistsName(connection);
-                        if (playlistsName.contains(oldPlaylistName)) {
+                        case 6: {
+                            System.out.println("Enter the playlist name you want to edit - ");
+                            scanner.nextLine();
+                            String oldPlaylistName = scanner.nextLine();
                             System.out.println("Enter the new playlist name - ");
                             String newPlaylistName = scanner.nextLine();
-                            boolean result = playlistRepository.editPlaylistName(connection, oldPlaylistName, newPlaylistName);
-                            if (result) {
-                                System.out.println("Playlist name updated successfully!");
-                            }
-                        } else {
-                            System.err.println("Playlist with the name " + oldPlaylistName + " does not exist!");
+                            playlistRepository.editPlaylistName(connection, oldPlaylistName, newPlaylistName);
+                            break;
                         }
-                    } else if (choice == 7) {
-                        System.out.println("Enter the Genre - ");
-                        scanner.nextLine();
-                        String genreName = scanner.nextLine();
-                        List<Song> searchedSongsByGenre = songRepository.searchSongsByGenre(allSongs, genreName);
-                        if (searchedSongsByGenre.size() != 0) {
+                        case 7:
+                            System.out.println("Enter the Genre - ");
+                            scanner.nextLine();
+                            String genreName = scanner.nextLine();
+                            List<Song> searchedSongsByGenre = songRepository.searchSongsByGenre(allSongs, genreName);
                             songRepository.displayAllSongs(searchedSongsByGenre);
-                        } else {
-                            System.err.println("No songs found with the genre " + genreName);
-                        }
-                    } else if (choice == 8) {
-                        System.out.println("Enter the Artist Name - ");
-                        scanner.nextLine();
-                        String artistName = scanner.nextLine();
-                        List<Song> searchedSongsByArtist = songRepository.searchSongsByArtist(allSongs, artistName);
-                        if (searchedSongsByArtist.size() != 0) {
+                            break;
+                        case 8:
+                            System.out.println("Enter the Artist Name - ");
+                            scanner.nextLine();
+                            String artistName = scanner.nextLine();
+                            List<Song> searchedSongsByArtist = songRepository.searchSongsByArtist(allSongs, artistName);
                             songRepository.displayAllSongs(searchedSongsByArtist);
-                        } else {
-                            System.err.println("No songs found with the genre " + artistName);
-                        }
+                            break;
+                        default:
+                            System.out.println("Invalid Input");
                     }
                 } while (choice != 9);
             } else {
