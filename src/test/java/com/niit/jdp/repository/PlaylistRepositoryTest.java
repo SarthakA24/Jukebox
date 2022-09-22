2c5024ae66ea7a1184bbcb9c033c3893ddbc61b0
Sarthak Agarwal <sarthakaggarwal24@gmail.com>
2022-09-22 13:51:46 +0530

    added test cases for PlaylistRepository

---
+++ src/test/java/com/niit/jdp/repository/PlaylistRepositoryTest.java
@@ -0,0 +1,24 @@
+package com.niit.jdp.repository;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class PlaylistRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getPlaylistsName() {
+    }
+
+    @Test
+    void getPlaylistByName() {
+    }
+}
